a0ce39068b58c549fa581ad36df857e2e7efb510
Aldair Garros <dev@aldairgarros.com>
2026-08-11 11:06:31 -0300

    refactor: remove project route and unused components

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./pages/Layout";
 import { Home } from "./pages/home";
-import { Project } from "./pages/project";
 
 export default function AppRouter() {
   const root = createBrowserRouter([
     {
       element: <RootLayout />,
-      children: [
-        { path: "/", element: <Home /> },
-        { path: "/projects/:project", element: <Project /> },
-      ],
+      children: [{ path: "/", element: <Home /> }],
     },
   ]);
 
